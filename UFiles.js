ec7afbacc6000b909eed82ae335229cad7fe1aa8
Christophe Hurpeau <christophe@hurpeau.com>
2013-04-21 21:11:54 +0200

    Yaml

--- UFiles.js
+++ UFiles.js
@@ -1,7 +1,13 @@
 /* NODE */
-var fs=require('fs');
+var fs=require('fs'),YAML=require('js-yaml');
 
 global.UFiles={
-	readJsonSync:function(file){ return JSON.parse(fs.readFileSync(file,'UTF-8')); }
+	readJsonSync:function(file){ return JSON.parse(fs.readFileSync(file,'UTF-8')); },
+	readYamlSync:function(file){
+		var content=fs.readFileSync(file,'UTF-8');
+		if(!content) return false;
+		//if(!content.startsWith('---')) content="---\n"+content;
+		return YAML.load(content.replace("\t",' '));
+	}
 };
 /* /NODE */
