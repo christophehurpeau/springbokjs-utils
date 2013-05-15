8ad56100fc475390c5c6e78a1d8102a3cc85293b
Christophe Hurpeau <christophe@hurpeau.com>
2013-05-15 10:27:11 +0200

    CallbacksOnce

---
+++ Callbacks/CallbacksOnce.js
@@ -0,0 +1,30 @@
+global.CallbacksOnce=function(){};
+CallbacksOnce.prototype={
+	on:function(listener){
+		var events = this._callbacks || (this._callbacks = []);
+		if(UArray.has(callbacks,listener)) return this;
+		this._callbacks.push(listener);
+		return this;
+	},
+	off:function(event,listener){
+		var i;
+		if(this._callbacks && (i=this._callbacks.indexOf(listener)))
+			this._callbacks.splice(i,1);
+		return this;
+	},
+	fire:function(args){
+		/*if(this._events[event]){
+			args = UArray.slice1(arguments);
+			for(var i=0,events=this._events[event],l=events.length; i<l; i++)
+				events[i].apply(this,args);
+		}*/
+		if(this._callbacks && this._callbacks.length!==0){
+			var i = this._callbacks.length,response;
+			while(i--){
+				response = this._callbacks[i].apply(null,args);
+				if(response===false) break;
+			}
+		}
+		return this;
+	}
+};
