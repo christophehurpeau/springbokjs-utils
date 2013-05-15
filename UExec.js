4686c2e6c3eccd9a2b16ee5198815e014294204e
Christophe Hurpeau <christophe@hurpeau.com>
2013-05-15 10:27:22 +0200

    UExec

---
+++ UExec.js
@@ -0,0 +1,12 @@
+/* NODE */
+var _exec=require('child_process').exec;
+
+global.UExec={
+	exec:function(cmd,callback){
+		_exec(cmd,callback);
+	},
+	escape:function(arg){
+		return '"'+arg.replace(/(["\s'$`\\])/g,'\\$1')+'"';
+	}
+};
+/* /NODE */
--- UFiles.js
+++ UFiles.js
@@ -8,6 +8,13 @@ global.UFiles={
 		if(!content) return false;
 		//if(!content.startsWith('---')) content="---\n"+content;
 		return YAML.load(content.replace("\t",' '));
+	},
+	readYamlAsync:function(file,callback){
+		fs.readFile(file,'UTF-8',function(err,content){
+			if(err) return callback(err);
+			if(!content) return callback(null,false);
+			callback(null,YAML.load(content.replace("\t",' ')));
+		});
 	}
 };
 /* /NODE */
