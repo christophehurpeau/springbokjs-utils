5009f4a3dfb13814752bd34780fbbf9a62e559ae
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-10 16:41:01 +0200

    0.5.8 request

---
+++ src/request.js
@@ -0,0 +1,29 @@
+var request = require('request');
+var promiseUtils = require('./promises');
+
+module.exports = function(uri, options) {
+    return promiseUtils.promiseCallback((done) => {
+        request(uri, options, function(error, response, body) {
+            done(error, response);
+        });
+    });
+};
+
+'defaults put patch post head del get'.split(' ').forEach((key) => {
+    module.exports[key] = function(uri, options) {
+        return promiseUtils.promiseCallback((done) => {
+            request(uri, options, function(error, response, body) {
+                done(error, response);
+            });
+        });
+    };
+});
+
+// copy request's properties
+for (var k in request) {
+    if (request.hasOwnProperty(k)) {
+        if (!module.exports[k]) {
+            module.exports[k] = request[k];
+        }
+    }
+}
