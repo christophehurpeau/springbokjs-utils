c5fe697cdef14088db5363e1e6b4cae71781d7da
Christophe Hurpeau <christophe@hurpeau.com>
2015-02-04 02:50:04 +0100

    1.0.1

--- src/php.js
+++ src/php.js
@@ -1,5 +1,11 @@
 var S = require('./index');
 
+if ( !String.prototype.contains ) {
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
+}
+
 /**
  * Php utils
  *
